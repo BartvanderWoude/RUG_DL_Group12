baee09187d52afb3e049c315865d6f5065f55ce6
Altripayo <b.r.van.der.woude@student.rug.nl>
2024-03-13 22:48:03 +0100

    Minor change to tests.

--- tests/tests.py
+++ tests/tests.py
@@ -8,21 +8,21 @@ from src.logger import Logger
 def test_traindataset():
     dataset = Fruits(file="utils/train_fruits.csv")
     sample = dataset[0]
-    assert sample.shape == (3, 100, 100)
-    assert len(dataset) == 67692
 
+    assert sample.shape == (3, 100, 100)
     assert torch.max(sample) <= 1.0
     assert torch.min(sample) >= -1.0
+    assert len(dataset) == 67692
 
 
 def test_testdataset():
     dataset = Fruits(file="utils/test_fruits.csv")
     sample = dataset[0]
-    assert sample.shape == (3, 100, 100)
-    assert len(dataset) == 22688
 
+    assert sample.shape == (3, 100, 100)
     assert torch.max(sample) <= 1.0
     assert torch.min(sample) >= -1.0
+    assert len(dataset) == 22688
 
 
 def test_logger():
